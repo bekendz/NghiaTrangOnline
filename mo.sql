fc8694a1bd5c377f2a805e388e1f072d11cf9483
bekendz <68892311+bekendz@users.noreply.github.com>
2020-09-16 14:33:49 +0700

    Add files via upload

--- mo.sql
+++ mo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 14, 2020 at 01:17 PM
+-- Generation Time: Sep 16, 2020 at 09:23 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.10
 
@@ -47,9 +47,10 @@ CREATE TABLE `mo` (
 --
 
 INSERT INTO `mo` (`ID`, `Thanh`, `TenNguoiKhuat`, `Email`, `NgayThangNamSinh`, `NgayThangNamMat`, `QueQuan`, `NguoiLapMo`, `MatKhau`, `Hinh`, `HinhBia`, `Dao`) VALUES
-(1, 'Phêrô', 'Nguyễn Văn Trương', 'php@gmail.com', '1954-09-12', '2015-10-23', 'Lâm Đồng', 'Con và các cháu', '123', '4.jpg', 'upload/anhthetest.jpg', 'Thiên Chúa'),
-(2, '', 'Nguyễn Văn Anh', 'vananh@gmail.com', '1965-12-27', '2017-12-24', 'Vĩnh Long', 'Bạn thân', '12345', '1.jpg', '', 'Đức Phật'),
-(3, 'Anrê', 'Trần Lâm Sơn', 'thuy@gmail.com', '1987-07-15', '2012-11-07', 'Cần Thơ', 'Con và các cháu', '123', '8.jpg', '', 'Thiên Chúa');
+(1, 'Phêrô', 'Nguyễn Văn Trương', 'php@gmail.com', '1954-09-12', '2015-10-21', 'Lâm Đồng', 'Con và các cháu', '123', '4.jpg', 'upload/anhthetest.jpg', 'Thiên Chúa'),
+(2, '', 'Nguyễn Văn Anh', 'vananh@gmail.com', '1965-12-27', '2017-08-11', 'Vĩnh Long', 'Bạn thân', '12345', '1.jpg', '', 'Đức Phật'),
+(3, 'Anrê', 'Trần Lâm Sơn', 'thuy@gmail.com', '1987-07-15', '2012-11-07', 'Cần Thơ', 'Con và các cháu', '123', '8.jpg', '', 'Thiên Chúa'),
+(4, '', 'Lâm Văn Độ', 'do@gmail.com', '1989-09-18', '2019-01-15', 'Vĩnh Long', 'Con và Mẹ', '14782', '1.jpg', '', 'Đức Phật');
 
 --
 -- Indexes for dumped tables
@@ -69,7 +70,7 @@ ALTER TABLE `mo`
 -- AUTO_INCREMENT for table `mo`
 --
 ALTER TABLE `mo`
-  MODIFY `ID` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ID` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
